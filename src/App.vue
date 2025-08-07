ec99a8bd40ebdf8dab6e59f33d15d3133a785a5e
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-07 22:45:08 +0600

    initialized

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeroSection from './zems/Front/Components/Section/HeroSection.vue';
+
+
+</script>
+
+<template>
+  <HeroSection/>
+</template>
+
+<style scoped></style>
---
+++ src/zems/Front/Components/Section/HeroSection.vue
@@ -0,0 +1,3 @@
+<template>
+  HeroSecton
+</template>
