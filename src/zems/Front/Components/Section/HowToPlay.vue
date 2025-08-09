41a217b03be1d1256e657a122375d2da3d152ea5
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-09 23:58:12 +0600

    update

--- src/zems/Front/Components/Section/HowToPlay.vue
+++ src/zems/Front/Components/Section/HowToPlay.vue
@@ -9,19 +9,19 @@ const steps = ref([
     id: 1,
     title: "Register & Join",
     description: "Sign up for the Ludo event online and secure your spot.",
-    icon: "https://cdn-icons-png.flaticon.com/512/60/60879.png"
+    icon: "/verify.png"
   },
   {
     id: 2,
     title: "Get Your Code",
     description: "Receive your unique player code and room link via email.",
-    icon: "https://cdn-icons-png.flaticon.com/512/60/60879.png"
+    icon: "/one-time-password.png"
   },
   {
     id: 3,
     title: "Join the Room",
     description: "Log in and join the Ludo match room at the scheduled time.",
-    icon: "https://cdn-icons-png.flaticon.com/512/60/60879.png"
+    icon: "/team.png"
   },
 
 ])
@@ -36,7 +36,7 @@ const steps = ref([
         <div v-for="(step, i) in steps" :key="step.id" class="timeline-card">
           <span class="number">{{ i }}</span>
           <BaseImage :image="step.icon" />
-          <SubTitle>{{step.title}}</SubTitle>
+          <SubTitle class="mt-1">{{step.title}}</SubTitle>
           <BaseParagraph>{{ step.description }}</BaseParagraph>
         </div>
 
