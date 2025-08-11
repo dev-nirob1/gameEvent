5086cbb39368798a5205479f5908cd08a3863657
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-11 15:49:11 +0600

    update accordion section update

--- src/zems/Front/Components/Section/FaqSection.vue
+++ src/zems/Front/Components/Section/FaqSection.vue
@@ -1,50 +1,59 @@
 <script setup>
-import BaseTitle from '@/components/Elements/BaseTitle.vue';
-import BaseParagraph from '@/components/Elements/BaseParagraph.vue';
 import { ref } from 'vue';
+import AccordionFaq from '../Widgets/AccordionFaq.vue';
 
 const faqs = ref([
   {
+    id: 1,
     question: 'How do I join a Ludo event?',
     answer: 'Simply register online for your preferred event, receive your player code, and join the match room at the scheduled time.',
     open: false
   },
   {
+    id: 2,
     question: 'Is there any entry fee?',
     answer: 'Most events are free to join, but special tournaments may have a small entry fee to compete for bigger prizes.',
     open: false
   },
   {
+    id: 3,
     question: 'Can I play on mobile?',
     answer: 'Yes! You can join and play from your mobile device or desktop without any issues.',
     open: false
   },
   {
+    id: 4,
     question: 'What if I miss the event time?',
     answer: 'Unfortunately, late joiners may not be allowed once the match starts. Please ensure you join on time.',
     open: false
   },
 ]);
 
-const toggleFAQ = (index) => {
-  faqs.value[index].open = !faqs.value[index].open;
+const currentOpenItem = ref();
+const toggleAccordion = (id) => {
+  if (currentOpenItem.value == id) {
+    currentOpenItem.value = null;
+  } else {
+    currentOpenItem.value = id;
+  }
 };
 </script>
 
 <template>
   <section class="faq-section">
     <div class="container">
-      <BaseTitle class="text-center mb-5">Frequently Asked Questions</BaseTitle>
+      <div class="medium-2 gap-2">
+        <div>
+          <BaseTitle>Frequently</BaseTitle>
+          <BaseTitle>Asked Question</BaseTitle>
+          <BaseParagraph>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quam incidunt ut eos
+            similique soluta ullam consequatur. Et labore beatae repellat? Alias optio dolorum quidem quis fuga illo
+            doloribus excepturi?</BaseParagraph>
+        </div>
 
-      <div class="faq-list">
-        <div v-for="(faq, i) in faqs" :key="i" class="faq-item">
-          <button class="faq-question" @click="toggleFAQ(i)">
-            {{ faq.question }}
-            <span class="icon">{{ faq.open ? '-' : '+' }}</span>
-          </button>
-          <BaseParagraph v-if="faq.open" class="faq-answer">
-            {{ faq.answer }}
-          </BaseParagraph>
+        <div>
+          <AccordionFaq v-for="(faq, i) in faqs" :key="i" :currentOpenItem="currentOpenItem" :accordionData="faq"
+            :toggleAccordion="toggleAccordion" />
         </div>
       </div>
     </div>
@@ -64,7 +73,7 @@ const toggleFAQ = (index) => {
 }
 
 .faq-item {
-  border-bottom: 1px solid rgba(255,255,255,0.1);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
   padding: 1rem 0;
 }
 
