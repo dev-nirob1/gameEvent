9d80a62c356530304ec5d7982b2acb799d4d3bc1
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-12 20:39:29 +0600

    update contact section

--- src/zems/Front/Components/Section/ContactSection.vue
+++ src/zems/Front/Components/Section/ContactSection.vue
@@ -2,30 +2,33 @@
   <section class="contact">
     <div class="container">
       <div class="medium-2 gap-2">
+        <div class="contact-info">
+          <BaseTitle class="">We're here to help</BaseTitle>
+          <BaseParagraph>Got a question about our tournaments or games? We’re here to help you roll your way to victory!
+          </BaseParagraph>
+          <ul>
+            <ListItem><i class="fa-solid fa-phone"></i> +880 1234 567 890</ListItem>
+            <ListItem><i class="fa-solid fa-envelope"></i> support@ludogame.com</ListItem>
+            <ListItem><i class="fa-brands fa-discord"></i> Join our Discord</ListItem>
+          </ul>
+        </div>
         <div>
-        <BaseTitle class="mb-2">We're here to help</BaseTitle>
-      </div>
-      <div>
-        <form>
-          <div>
-            <label for="name">Name: </label>
-            <InputField id="name" type="text" placeholder="Enter Your Name" />
-          </div>
-          <div>
-            <label for="email">Email: </label>
-            <InputField id="email" type="email" placeholder="Enter Your Email" />
-          </div>
-          <div>
-            <label for="phone">Phone: </label>
-            <InputField type="tel" id="phone" placeholder="Enter Your Phone Number" />
-          </div>
-          <div>
-            <label for="message">Message: </label>
-            <BaseTextArea rows="6" id="message" placeholder="Enter Message..."></BaseTextArea>
-          </div>
-          <BaseButton class="bg-secondary width-full">Send Message</BaseButton>
-        </form>
-      </div>
+          <form>
+            <div>
+              <label for="name">Name: </label>
+              <InputField id="name" type="text" placeholder="Enter Your Name" />
+            </div>
+            <div>
+              <label for="email">Email: </label>
+              <InputField id="email" type="email" placeholder="Enter Your Email" />
+            </div>
+            <div>
+              <label for="message">Message: </label>
+              <BaseTextArea rows="6" id="message" placeholder="Enter Message..."></BaseTextArea>
+            </div>
+            <BaseButton class="bg-secondary width-full">Send Message</BaseButton>
+          </form>
+        </div>
       </div>
     </div>
   </section>
@@ -34,6 +37,20 @@
 .contact {
   padding: 3.75rem 0;
   background: url('background.svg') center / cover no-repeat fixed;
+}
+
+.contact .contact-info ul {
+  list-style: none;
+  padding: 0;
+  margin-top: 1.5rem;
+}
+
+.contact .contact-info ul li {
+  margin-top: .5rem;
+}
+
+.contact .contact-info ul li i{
+  color: var(--secondary-color);
 }
 
 input,
@@ -46,8 +63,12 @@ textarea {
   color: var(--white-color);
 }
 
-
 .contact form label {
   margin: .5rem .25rem;
+}
+
+.contact .btn {
+  border-color: var(--secondary-color);
+  color: var(--white-color);
 }
 </style>
