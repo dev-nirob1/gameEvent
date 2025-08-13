9689bc94949814d34ccb12e914a943d11d1300cd
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-13 20:08:26 +0600

    install pwa

--- src/zems/Front/Pages/LeaderBoard.vue
+++ src/zems/Front/Pages/LeaderBoard.vue
@@ -56,6 +56,6 @@ const leaderboard = [
 <style scoped>
 .leaderboard {
   padding: 3.5rem 0;
-  background: url('background.svg') center / cover no-repeat fixed;
+  background: url('/background.svg') center / cover no-repeat fixed;
 }
 </style>
