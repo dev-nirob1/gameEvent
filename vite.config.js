51b17d7df6755139bd76d558e6c33b0c7d49e50b
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-14 00:20:26 +0600

    update logo in manifest

--- vite.config.js
+++ vite.config.js
@@ -12,28 +12,61 @@ export default defineConfig({
     vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate',
-       devOptions: {
-        enabled: true
+      devOptions: {
+        enabled: true,
       },
       manifest: {
+        id: '/',
         name: 'Ludo Clash',
         short_name: 'LudoC',
         description: 'Compete with players worldwide in the ultimate Ludo showdown!',
         theme_color: '#3b82f6',
         background_color: '#0C0C1D',
         display: 'standalone',
+        orientation: 'landscape',
+        display_override: ['window-controls-overlay', 'standalone'],
         start_url: '/',
-        icons: {
-          src: '/logo.png',
-          size: '512x512',
-          type: 'image/png'
-        }
-      }
-    })
+        icons: [
+          {
+            src: '/mobile-logo-1.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/mobile-log.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+        ],
+        screenshots:[
+          {
+            src: '/desktop.png',
+            sizes: '2560x1440',
+            type: 'image/png',
+            label: 'Ludo clash gameplay',
+            form_factor: 'wide'
+          },
+          {
+            src: '/mobile.png',
+            sizes: '750x1334',
+            type: 'image/png',
+            label: 'Ludo clash mobile view',
+          },
+        ]
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
