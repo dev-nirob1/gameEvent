c4fb311151b337f90fee17d2ede881e3f71b64eb
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-16 17:12:22 +0600

    update breadcrumb section

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ntl0l4bops"
+    "revision": "0.6fp0qsg5qf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Widgets/BreadCrumb.vue
+++ src/components/Widgets/BreadCrumb.vue
@@ -8,59 +8,93 @@ const route = useRoute()
 
 <template>
   <section>
-    <!-- Desktop Breadcrumb -->
-    <div class="breadcrumb">
-      <ul class="container">
-        <ListItem><RouterLink to="/">Home</RouterLink></ListItem>
+    <div class="container">
+      <ul class="mobile-breadcrumb">
+        <ListItem>
+          <BaseButton><i class="fa-solid fa-angle-left"></i></BaseButton>
+        </ListItem>
         <ListItem>{{ route.name }}</ListItem>
-        <ListItem v-if="route.params.slug">{{ route.params.slug }}</ListItem>
       </ul>
+      <!-- Desktop Breadcrumb -->
+      <div class="breadcrumb">
+        <ul class="container">
+          <ListItem>
+            <RouterLink to="/">Home</RouterLink>
+          </ListItem>
+          <ListItem>{{ route.name }}</ListItem>
+          <ListItem v-if="route.params.slug">{{ route.params.slug }}</ListItem>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-
-
 .breadcrumb {
-  padding: 8rem 0 3rem 0;
+  display: none;
 }
-
-.breadcrumb ul {
+ul {
   list-style: none;
   padding: 0;
-  font-size: 1rem;
+  margin: 0;
+}
+.mobile-breadcrumb {
+  padding: 1rem;
   display: flex;
   align-items: center;
   gap: 1rem;
 }
 
-.breadcrumb ul li a {
-  position: relative;
-  color: var(--primary-light-color);
-  text-decoration: none;
-  padding-bottom: .5rem;
+.mobile-breadcrumb .btn {
+  color: var(--white-color);
+  padding: .25rem;
+  border: none;
+  margin: 0;
 }
 
-.breadcrumb ul li a::after {
-  content: "";
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  height: 2px;
-  width: 0;
-  background-color: var(--secondary-color);
-  transition: all .3s ease-in-out;
-}
+@media (min-width: 768px) {
+  .mobile-breadcrumb {
+    display: none;
+  }
 
-.breadcrumb ul li a:hover::after {
-  width: 100%;
-}
+  .breadcrumb {
+    display: block;
+    padding: 8rem 0 2rem 0;
+  }
 
-.breadcrumb ul li:not(:last-child)::after {
-  content: ">";
-  font-family: monospace;
-  padding-left: 1rem;
-}
+  .breadcrumb ul {
+    font-size: 1.25rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
 
+  .breadcrumb ul li a {
+    position: relative;
+    color: var(--primary-light-color);
+    text-decoration: none;
+    padding-bottom: .5rem;
+  }
+
+  .breadcrumb ul li a::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    height: 2px;
+    width: 0;
+    background-color: var(--secondary-color);
+    transition: all .3s ease-in-out;
+  }
+
+  .breadcrumb ul li a:hover::after {
+    width: 100%;
+  }
+
+  .breadcrumb ul li:not(:last-child)::after {
+    content: ">";
+    font-family: monospace;
+    padding-left: 1rem;
+  }
+}
 </style>
