a7d5abdd80717652b82cc59ce8f93d8e43a6c796
dev-nirob1 <dev.nirob1@gmail.com>
2025-10-20 22:29:50 +0600

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.klc9tvecufo"
+    "revision": "0.9234hm8872g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
